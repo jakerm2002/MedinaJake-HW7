c19f1cc9259878a865fcae89fe2da05a4ad3dad6
Jake Medina <jake.medina02@gmail.com>
2023-10-23 22:00:19 -0500

    Initial Commit

---
+++ MedinaJake-HW7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedinaJake-HW7
+//
+//  Created by Jake Medina on 10/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
